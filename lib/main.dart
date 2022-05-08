c395977116e4ac3b912563e01e5267b8d6b84309
AiBiC-Data <whrnjsgh12@naver.com>
2022-05-08 13:50:27 +0900

    updating stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,24 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
   var questions = [
     'What\' your favorite color?',
     'What\'s your favorite animals?',
@@ -14,26 +31,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('My First App'),
-          ),
-          body: Column(
-            children: [
-              Text('The question!'),
-              RaisedButton(
-                child: Text('Answer1'),
-                onPressed: null,
-              ),
-              RaisedButton(
-                child: Text('Answer2'),
-                onPressed: null,
-              ),
-              RaisedButton(
-                child: Text('Answer3'),
-                onPressed: null,
-              ),
-            ],
-          )),
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text(
+              questions[questionIndex],
+            ),
+            RaisedButton(
+              child: Text('Answer1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer2'),
+              onPressed: () => print('Answer2 choosen'),
+            ),
+            RaisedButton(
+              child: Text('Answer3'),
+              onPressed: () {
+                // ...
+                print('Answer3 choosen');
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
